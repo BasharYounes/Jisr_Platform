95a853d792c63f59c4e993fcc0a67bab4f86576d
BasharYounes <bywns3777@gmail.com>
2026-05-03 23:33:31 +0300

    add CV

--- app/Models/Cv.php
+++ app/Models/Cv.php
@@ -3,18 +3,45 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Cv extends Model
+class CV extends Model
 {
-    protected $guarded = [];
+    protected $table = 'c_v_s';
+    protected $primaryKey = 'CvID';
+
+    protected $fillable = [
+        'UserId',
+        'FileUrl',
+        'IsPrimary',
+        'UploadedAt',
+    ];
+
+    protected $casts = [
+        'IsPrimary' => 'boolean',
+        'UploadedAt' => 'datetime',
+    ];
+
+    public $timestamps = false;
+
+    // public function analysis(): HasOne
+    // {
+    //     return $this->hasOne(CVAnalysis::class, 'CvId', 'CvID');
+    // }
+
+    public function assessmentSessions(): HasMany
+    {
+        return $this->hasMany(AssessmentSession::class, 'CvID', 'CvID');
+    }
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'UserId', 'id');
     }
 
-    public function analyses()
+    public function getRouteKeyName(): string
     {
-        return $this->hasMany(CvAnalysis::class);
+        return 'CvID';
     }
+
 }
