f491226d825f1a0865e8a409e52b889103f87041
Batoul <batoulsubuh@gmail.com>
2026-06-16 02:30:19 +0300

    Get Task By Filter

--- routes/api.php
+++ routes/api.php
@@ -121,6 +121,7 @@ Route::get('/company/home', [CompanyHomeController::class, 'index'])->middleware
 // Company Tasks Creation & Publish
 Route::middleware(['auth:sanctum', 'role:company'])->prefix('company/tasks')->controller(CompanyTaskController::class)->group(function () {
     Route::post('index/', 'index');
+    Route::get('', 'index');
     Route::post('/', 'store');
     Route::get('/{taskId}', 'show')->whereNumber('taskId');
     Route::put('/{taskId}', 'update')->whereNumber('taskId');
